a2421cd3dad4201d13abeead1736d2ae9a534193
ntlam2061999 <nguyentunglam2061999@gmail.com>
2022-05-26 21:44:08 +0700

    ntlam

--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -9,9 +9,7 @@ import "./Product.css";
 const Product = ({ id, name, url }) => {
   return (
     <div className="col-xs-2 col-sm-2 col-md-4 col-lg-6 col-xl-6">
-      {/* product-item */}
       <div className="product-container">
-        {/* item-image */}
         <div
           className="img-container"
           style={{ backgroundImage: `url(${url})` }}
