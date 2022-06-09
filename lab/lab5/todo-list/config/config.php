d6dce5e1552e35399a47f79001cf46ca2fac57ab
qmanht06 <fbittq@gmail.com>
2022-06-09 21:39:06 +0700

    lab5

---
+++ lab/lab5/todo-list/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/** Configuration Variables **/
+
+define ('DEVELOPMENT_ENVIRONMENT',true);
+
+define('DB_NAME', 'lab5');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost:3306');
