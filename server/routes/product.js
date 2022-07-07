a60d2a39c3c2f325898fa2272bcee867a448067d
Mạnh Triệu <fbittq@gmail.com>
2022-07-07 13:30:00 +0700

    fix loi crete product

--- server/routes/product.js
+++ server/routes/product.js
@@ -7,14 +7,14 @@ const Products = require('../models/Product')
 // desc create Product
 // access private
 router.post('/', async (req, res) => {
-    const { productName, productImage, productDescription, productPrice } = req.body
+    const { productName, productImage, productDescription, productPrice, productCategory } = req.body
 
     // simple validation
-    if (!productName || !productImage || !productDescription || !productPrice)
+    if (!productName || !productImage || !productDescription || !productPrice || !productCategory)
         return res.status(400).json({ success: false, message: 'All is requires' })
 
     try {
-        const newProduct = new Products(productName, productImage, productDescription, productPrice)
+        const newProduct = new Products({ productName, productImage, productDescription, productPrice, productCategory, user: '6296db51a2628ee670ae2a59' })
 
         await newProduct.save()
         return res.status(500).json({
